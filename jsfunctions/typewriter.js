08e300e45a51c03ca7e70ce9680b00855256ccbb
KceeM <141727280+KceeM@users.noreply.github.com>
2024-10-10 20:47:22 +0200

    pro

---
+++ jsfunctions/typewriter.js
@@ -0,0 +1,23 @@
+// The text to animate
+const typewriterText = "Kaycee Chantelle, and I am passionate about living a plant-based lifestyle. Here’s why...";
+
+//animates the text
+function typeWriterEffect(text, elementId, speed) {
+    const d3Element = d3.select(elementId);
+    let index = 0;
+    
+    function type() {
+        if (index < text.length) {
+            d3Element.text(d3Element.text() + text.charAt(index));
+            index++;
+            setTimeout(type, speed);
+        }
+    }
+    
+    type();
+}
+
+// function on window load or after the DOM is ready
+window.onload = function() {
+    typeWriterEffect(typewriterText, "#typewriter", 100); // Adjust speed (100ms per character)
+};
