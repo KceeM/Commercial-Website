f7b390d3356f2397a13a330b1aa6c49d7ec3d0d1
KceeM <141727280+KceeM@users.noreply.github.com>
2024-10-10 13:43:12 +0200

    bars

--- jsfunctions/bargraph.js
+++ jsfunctions/bargraph.js
@@ -5,62 +5,114 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error("Error loading data:", error));
 });
 
-function createBarGraph(data) {
-    const margin = {top: 30, right: 30, bottom: 70, left: 60};
+function createBarGraphs(yearlyData, productData) {
+    const margin = { top: 20, right: 30, bottom: 60, left: 60 };
     const width = 800 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+    const height = 400 - margin.top - margin.bottom;
 
-    const svg = d3.select("#bar-graph-container")
+    // Graph 1: Sales Growth by Year
+    const svg1 = d3.select("#yearly-sales-graph")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    const x = d3.scaleBand()
-        .domain(data.map(d => d.productType))
+    const x1 = d3.scaleBand()
+        .domain(yearlyData.map(d => d.year))
         .range([0, width])
-        .padding(0.2);
+        .padding(0.1);
 
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.forecast)])
+    const y1 = d3.scaleLinear()
+        .domain([0, d3.max(yearlyData, d => d.sales)])
         .nice()
         .range([height, 0]);
 
-    svg.append("g")
-        .attr("class", "x-axis")
+    svg1.append("g")
         .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "end");
-
-    svg.append("g")
-        .attr("class", "y-axis")
-        .call(d3.axisLeft(y));
-
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.productType))
-        .attr("y", d => y(d.sales))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(d.sales))
-        .attr("fill", "green")
-        .on("mouseover", function(event, d) {
-            d3.select(this).style("fill", "orange");
-        })
-        .on("mouseout", function(event, d) {
-            d3.select(this).style("fill", "green");
-        });
-
-    svg.selectAll(".label")
-        .data(data)
-        .enter().append("text")
-        .attr("class", "label")
-        .attr("x", d => x(d.productType) + x.bandwidth() / 2)
-        .attr("y", d => y(d.sales) - 10)
+        .call(d3.axisBottom(x1))
+        .append("text")
+        .attr("y", 40)
+        .attr("x", width / 2)
         .attr("text-anchor", "middle")
-        .text(d => `$${(d.sales / 1e9).toFixed(1)}B`);
+        .attr("fill", "black")
+        .text("Year");
+
+    svg1.append("g")
+        .call(d3.axisLeft(y1))
+        .append("text")
+        .attr("y", -50)
+        .attr("x", -height / 2)
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .text("Sales (in billion USD)");
+
+    svg1.selectAll(".bar")
+        .data(yearlyData)
+        .enter().append("rect")
+        .attr("x", d => x1(d.year))
+        .attr("y", d => y1(d.sales))
+        .attr("width", x1.bandwidth())
+        .attr("height", d => height - y1(d.sales))
+        .attr("class", "bar");
+
+    // Graph 2: Product Sales and Forecast
+    const svg2 = d3.select("#product-sales-forecast-graph")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const x2 = d3.scaleBand()
+        .domain(productData.map(d => d.productType))
+        .range([0, width])
+        .padding(0.1);
+
+    const y2 = d3.scaleLinear()
+        .domain([0, d3.max(productData, d => d.forecast)])
+        .nice()
+        .range([height, 0]);
+
+    svg2.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x2))
+        .append("text")
+        .attr("y", 40)
+        .attr("x", width / 2)
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .text("Product Type");
+
+    svg2.append("g")
+        .call(d3.axisLeft(y2))
+        .append("text")
+        .attr("y", -50)
+        .attr("x", -height / 2)
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .text("Sales (in billion USD)");
+
+    // Bars for sales and forecast
+    svg2.selectAll(".bar-sales")
+        .data(productData)
+        .enter().append("rect")
+        .attr("x", d => x2(d.productType))
+        .attr("y", d => y2(d.sales))
+        .attr("width", x2.bandwidth() / 2)
+        .attr("height", d => height - y2(d.sales))
+        .attr("class", "bar-sales")
+        .attr("fill", "green");
+
+    svg2.selectAll(".bar-forecast")
+        .data(productData)
+        .enter().append("rect")
+        .attr("x", d => x2(d.productType) + x2.bandwidth() / 2)
+        .attr("y", d => y2(d.forecast))
+        .attr("width", x2.bandwidth() / 2)
+        .attr("height", d => height - y2(d.forecast))
+        .attr("class", "bar-forecast")
+        .attr("fill", "orange");
 }
